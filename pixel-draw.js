9a1041962402c6347632abe86601eb9a5fb0b16a
n-dett <124851780+n-dett@users.noreply.github.com>
2023-04-14 21:39:04 -0500

    Add draw functionality and clear button

--- pixel-draw.js
+++ pixel-draw.js
@@ -1,7 +1,7 @@
 let gridContainer = document.querySelector('.grid-container');
-let gridNum = 22;
+let gridNum = 16;
 
-
+// Create initial grid
 function createGrid() {
   for(i = 0; i < Math.pow(gridNum, 2); i++){
     let gridSquare = document.createElement('div');
@@ -12,3 +12,30 @@ function createGrid() {
 }
 
 createGrid();
+
+let gridSquare = document.querySelectorAll('.grid-squares');
+let currentColor = '#0066ff';
+
+function draw() {
+  gridSquare.forEach(pixel => {
+    pixel.addEventListener('mousedown', changeColor);
+    pixel.addEventListener('mousemove', changeColor);
+  })
+}
+
+function changeColor() {
+  gridSquare.forEach(() => {
+      this.style.backgroundColor = currentColor
+  }
+)}
+
+function clearGrid() {
+  let clearButton = document.querySelector('#clear-button');
+  clearButton.addEventListener('click', () => {
+    gridSquare.forEach(pixel => {
+      pixel.style.backgroundColor = '#ffffff';
+    })
+  })
+}
+clearGrid();
+draw();
