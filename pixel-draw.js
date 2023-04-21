2298d675ce7879661cc80c6271ecaaa6714f4d9e
n-dett <124851780+n-dett@users.noreply.github.com>
2023-04-20 21:16:32 -0500

    Add grid size slider and fix event listeners
    Added a slider so user can change grid size. Moved the 'draw' event
    listeners inside createGrid() so they get added to all new divs.

--- pixel-draw.js
+++ pixel-draw.js
@@ -1,7 +1,31 @@
+// Set initial grid
 const gridContainer = document.querySelector('.grid-container');
-let gridNum = 16;
+const gridSlider = document.querySelector('#grid-slider')
+const sliderLabel = document.querySelector('#slider-label');
 
-// Create initial grid
+sliderLabel.textContent = gridSlider.value;
+let gridNum = 16
+
+createGrid();
+
+
+// Change grid size
+gridSlider.addEventListener('input', () => {
+  sliderLabel.textContent = gridSlider.value;
+  gridNum = gridSlider.value;
+  removeSquares(gridContainer);
+  createGrid();
+  // draw();
+})
+
+function removeSquares(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+
+// Create grid
 function createGrid() {
   for(i = 0; i < Math.pow(gridNum, 2); i++){
     let gridSquare = document.createElement('div');
@@ -9,13 +33,35 @@ function createGrid() {
     gridSquare.style.width = `calc(100% / ${gridNum})`;
     gridContainer.appendChild(gridSquare);
   }
+  const gridSquares = document.querySelectorAll('.grid-squares');
+
+  gridSquares.forEach(square => {
+    square.addEventListener('mousedown', changeColor)
+  })
+
+  gridContainer.addEventListener('mousedown', () => {
+    gridSquares.forEach(square => {
+      square.addEventListener('mouseover', changeColor);
+    });
+  });
+
+  gridContainer.addEventListener('mouseup', () => {  
+    gridSquares.forEach(square => {
+      square.removeEventListener('mouseover', changeColor);
+    });
+  });
+
+  function changeColor() {
+    this.style.backgroundColor = penColor;
+  }
+
+  clearGrid();
 }
 
-createGrid();
 
 // Choose pen color
 let penColor = '#000000';
-let choosePen = document.querySelector('#pen-button');
+let penButton = document.querySelector('#pen-button');
 
 const colorPicker = document.querySelector("#colorpicker")
 colorPicker.addEventListener("input", function(event){
@@ -23,46 +69,52 @@ colorPicker.addEventListener("input", function(event){
   console.log(penColor);
 });
 
-choosePen.addEventListener('click', (event) => {
+// Select pen tool
+penButton.addEventListener('click', () => {
   penColor = colorPicker.value;
 })
 
 
-// Draw
-const gridSquare = document.querySelectorAll('.grid-squares');
+// Draw on mousedown or mousedown and drag
 
-gridSquare.forEach(square => {
-  square.addEventListener('mousedown', changeColor)
-})
+// const gridSquares = document.querySelectorAll('.grid-squares');
 
-gridContainer.addEventListener('mousedown', () => {
-  gridSquare.forEach(square => {
-    square.addEventListener('mouseover', changeColor);
-  });
-});
+// gridSquares.forEach(square => {
+//   square.addEventListener('mousedown', changeColor)
+// })
 
-gridContainer.addEventListener('mouseup', () => {  
-  gridSquare.forEach(square => {
-    square.removeEventListener('mouseover', changeColor);
-  });
-});
+// gridContainer.addEventListener('mousedown', () => {
+//   gridSquares.forEach(square => {
+//     square.addEventListener('mouseover', changeColor);
+//   });
+// });
 
-function changeColor() {
-  this.style.backgroundColor = penColor;
-}
+// gridContainer.addEventListener('mouseup', () => {  
+//   gridSquares.forEach(square => {
+//     square.removeEventListener('mouseover', changeColor);
+//   });
+// });
 
-// Eraser tool
+// function changeColor() {
+//   this.style.backgroundColor = penColor;
+// }
+
+
+// Select eraser tool
 let erase = document.querySelector('#eraser-button');
 erase.addEventListener('click', () => penColor = '#ffffff')
 
-// Reset grid to white
+
+// Clear grid button
 function clearGrid() {
+  const gridSquares = document.querySelectorAll('.grid-squares');
   let clearButton = document.querySelector('#clear-button');
+  
   clearButton.addEventListener('click', () => {
-    gridSquare.forEach(square => {
+    gridSquares.forEach(square => {
       square.style.backgroundColor = '#ffffff';
     })
   })
 }
 
-clearGrid();
+
